d229bd6e0d2c13084c6a0bcc775c6c84fa91b4e2
techkgosi <techkgosi@gmail.com>
2026-04-08 17:44:26 +0200

    certifications

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+let currentSlide = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelectorAll(".carousel-item");
+
+    slides[currentSlide].classList.remove("active");
+
+    currentSlide += direction;
+
+    if (currentSlide < 0) {
+        currentSlide = slides.length - 1;
+    }
+
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+
+    slides[currentSlide].classList.add("active");
+}
